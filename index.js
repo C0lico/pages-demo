c8e4e654d3ca00d05f750f7b28ad39ab4b00f00f
ChenZhouzhen <952925323@qq.com>
2022-10-28 14:22:37 +0800

    Add simple code

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: XuanxuanVI
+ * @Date: 2022-10-28 14:20:58
+ * @LastEditTime: 2022-10-28 14:21:05
+ * @LastEditors: XuanxuanVI
+ * @Description: 
+ * @FilePath: \CapstoneProject\index.js
+ * Copyleft © XuanxuanVI
+ */
+window.onload = function() {
+    document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+}
